fdeadc04522865c59eec70469b9c8050cd788bb5
Muhammad Haris Rahat <altafharis87@gmail.com>
2023-09-07 03:31:08 +0500

    index.html

---
+++ app.js
@@ -0,0 +1,143 @@
+// Question # 1
+
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+// var total = num1 + num2
+// console.log(total)
+// document.write("The sum of " + num1 + " and " + num2 + " is " + total)
+
+
+// Question # 2
+
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+// var total = num1 - num2
+// console.log(total)
+// document.write("The subtraction of " + num1 + " from " + num2 + " is " + total)
+
+
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+// var total = num1 * num2
+// console.log(total)
+// document.write("The product of " + num1 + " and " + num2 + " is " + total)
+
+
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+// var total = num1 / num2
+// console.log(total)
+// document.write("The division of " + num1 + " from " + num2 + " is " + total)
+
+
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+// var total = num1 % num2
+// console.log(total)
+// document.write("The modulus of " + num1 + " from " + num2 + " is " + total)
+
+
+// Question # 3
+
+
+// var initialValue = + prompt("Enter Initial Value:")
+// var incrementedValue = initialValue + 1
+// var valueAfterAddition = incrementedValue + 7
+// var decrementedValue = valueAfterAddition - 1
+// var remainder = decrementedValue % 3
+// console.log(remainder)
+// document.write("Initial Value :" + initialValue + "<br>" + "Value after increment is : " + incrementedValue + "<br>" + "Value after addition is: " + valueAfterAddition + "<br>" + "Value after decrement is : " + decrementedValue + "<br>" + "The remainder is:" + remainder)
+
+
+
+// Question # 4
+
+
+// var ticketPrice = 600
+// var totalTicket = +prompt("Enter total numbers of ticket:")
+// var totalPrice = ticketPrice * totalTicket
+// console.log(totalPrice)
+// document.write("Total cost to buy " + totalTicket + " tickets to a movie is " + totalPrice +"PKR")
+
+
+// Question # 6
+
+// var celsiusTemp = +prompt("Enter temperature in celsius")
+// var celsiusConver = (celsiusTemp * 9/5) +32
+// console.log(celsiusConver)
+// var fahrenhiteTemp = +prompt("Enter temperature in fahrenhite")
+// var fahrenhiteConver = (fahrenhiteTemp - 32) * 5/9
+// console.log(fahrenhiteConver)
+// document.write(celsiusTemp+ "<sup>o</sup>C" + " is " + celsiusConver+"<sup>o</sup>F" +"<br>"+ fahrenhiteTemp + "<sup>o</sup>F" + " is " + fahrenhiteConver + "<sup>o</sup>C")
+
+
+
+// Question # 7
+
+
+// var priceOfItem1 = +prompt("Enter Price Of Item 1")
+// var quantityOfItem1 = +prompt("Enter Quantity Of Item 1")
+// var totalPriceOfItem1 = priceOfItem1 * quantityOfItem1
+// var priceOfItem2 = +prompt("Enter Price Of Item 2")
+// var quantityOfItem2 = +prompt("Enter Quantity Of Item 2")
+// var totalPriceOfItem2 = priceOfItem2 * quantityOfItem2
+// var shippingCharges = +prompt("Enter Shipping Charges")
+// var totalAmount = totalPriceOfItem1 + totalPriceOfItem2 + shippingCharges
+// console.log(totalAmount)
+// document.write("Price of item 1 is " + priceOfItem1 + "<br>" + "Quantity of item 1 is " + quantityOfItem1 + "<br>" + "Price of item 2 is " + priceOfItem2 + "<br>" + "Quantity of item 2 is " + quantityOfItem2 + "<br>" + "Shipping Charges " + shippingCharges + "<br>" + "Total Amount is " + totalAmount)
+
+
+// Question # 8
+
+// var totalMarks = +prompt("Enter Total Marks:")
+// var obtainedMarks = +prompt("Enter Obtained Marks:")
+// var percentage = (obtainedMarks / totalMarks) * 100
+// console.log(percentage)
+// document.write("Total Marks : " + totalMarks + "<br>" + "Obtained Marks : " + obtainedMarks + "<br>" + "Percentage : " + percentage )
+
+
+// Question # 9
+
+// var USdollar = + prompt("Enter Amount in US dollar:")
+// var SaudiRiyal = + prompt("Enter Amount in Saudi riyal:")
+// var USdollarCalculation = USdollar * 307.50
+// var SaudiRiyalCalculation = SaudiRiyal * 81.98
+// var totalPakistaniRupee = USdollarCalculation + SaudiRiyalCalculation
+// console.log(totalPakistaniRupee)
+// document.write("Total Currency in PKR : " + totalPakistaniRupee)
+
+
+// Question # 10
+
+// var randomNumber = + prompt ("Enter a randon number :")
+// var arithmaticOperation = (randomNumber + 5) * 10 / 2
+// console.log(arithmaticOperation)
+// document.write("Final answer is " + arithmaticOperation)
+
+
+// Question # 11
+
+// var currentYear = + prompt("Enter current year : ")
+// var birthYear = + prompt("Enter birth year : ")
+// var age = currentYear - birthYear
+// console.log(age)
+// document.write("Current year :" + currentYear + "<br>" + "Birth year : " + birthYear + "<br>" + "You are either " + (age-1) + " or " + age + " years old")
+
+
+// Question # 12
+
+// var radius = + prompt("Enter Radius: ")
+// var circumference = 2 * 3.142 * radius
+// var area = 3.142 * (radius)**2
+// console.log(circumference , area)
+// document.write("Radius of a circle is :" + radius + "<br>" + "The circumferece is :" + circumference + "<br>" + "The area is: " + area)
+
+// Question # 13
+
+// var favSnack = prompt("Enter Your Favourite snack : ")
+// var currentAge = + prompt ("Enter your current age: ")
+// var maximumAge = + prompt ("Enter your maximum age: ")
+// var perDaySnack = + prompt("Enter Amount : ")
+// var totalConsumption = (maximumAge - currentAge) * perDaySnack
+// console.log(totalConsumption)
+// document.write("Favourite snacks : " + favSnack + "<br>" + "Current Age : " + currentAge + "<br>" + "Maximum Age: " + maximumAge + "<br>" + "Snacks per day : " + perDaySnack+ "<br>" + "You will need " + totalConsumption  +  favSnack + " to last you until the rip age of " + maximumAge)
